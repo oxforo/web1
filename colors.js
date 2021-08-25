8224d6cf3aea16e067ca305ee9112d1d7017a695
Chae Su Min <oxforo@naver.com>
2021-08-25 23:46:07 +0900

    Add files via upload
    Javascript - 함수, 객체, 파일 단위의 구분

---
+++ colors.js
@@ -0,0 +1,35 @@
+var Links ={
+    setColor: function (color){
+    var alist = document.querySelectorAll('a');
+        var i =0
+        while(i < alist.length){
+            console.log(alist[i]);
+            alist[i].style.color = color;
+            i = i+1;
+        }
+    }
+}
+var Body = {
+    setColor: function (color){
+        document.querySelector('body').style.color= color;
+    },
+    setBackgroundColor: function (color){
+        document.querySelector('body').style.backgroundColor= color;
+    }
+}
+
+function nightDayHandler(self){
+    if( self.value == 'day'){
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        Links.setColor('powderblue');
+        self.value = 'night';
+
+    } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        Links.setColor('blue');
+        self.value = 'day';
+        
+    }
+}
